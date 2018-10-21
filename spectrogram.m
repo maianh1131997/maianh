83cdf2ed07aa1cee01930cebcfccbf8074ab015c
maianh1131997 <44335526+maianh1131997@users.noreply.github.com>
2018-10-21 23:08:03 +0700

    Update spectrogram.m

--- spectrogram.m
+++ spectrogram.m
@@ -3,7 +3,7 @@ win = 128 % window length in samples
 hop = win/2            
 
 nfft = win % width of each frequency bin 
-spectrogram(y,win,hop,nfft,fs,'yaxis')
+spectrogram(z,win,hop,nfft,fs,'yaxis')
 
 % change the tick labels of the graph from scientific notation to floating point: 
 yt = get(gca,'YTick');  
